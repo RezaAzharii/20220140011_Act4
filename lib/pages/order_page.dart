1fd5422639ba52eeceb01cc147b1adc2463fcddd
RezaAzharii <rezaazhari867@gmail.com>
2025-04-23 17:36:53 +0700

    Menambahkan parameters, controller, dan key untuk orderan

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -8,6 +8,13 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  int jumlahMakanan = 0;
+  int jumlahMinuman = 0;
+  int totalHarga = 0;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
